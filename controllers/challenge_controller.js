d712eb090ff77d4d990fa93b3b630f4f5f49fead
DESKTOP-HGEAK0F\Jonathan <dajoos.sistemacontable@gmail.com>
2022-06-16 08:28:33 -0500

    Agregando validaciones

--- controllers/challenge_controller.js
+++ controllers/challenge_controller.js
@@ -4,13 +4,21 @@ const {services} = require('../helpers/challengedCalculate')
 
 const challengeGet = async(req = request, res = response) => {
     const result = Object.values(data).flat();
+
     const dates = req.query;
+
     const basePrice = result.reduce((acc, el)=> parseFloat(acc) + parseFloat(el.amount),0 );
+
     const IVA = basePrice * 0.12;
+
     const commissions = basePrice > 100 ? (basePrice * 0.01) : 0;
+
     const insurance =  basePrice > 100 ? parseFloat((basePrice * 0.05 * -1).toFixed(2)) : 0;
+
     const service = services(basePrice, result.length);
+
     const total  = parseFloat((basePrice + IVA + commissions + insurance + service).toFixed(2));
+    
     res.json({
       dates,
       basePrice,
--- routes/challenge.js
+++ routes/challenge.js
@@ -1,9 +1,15 @@
 
 const { Router } = require('express');
 const {challengeGet} = require('../controllers/challenge_controller')
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 
-router.post('/', challengeGet );
+router.post('/',[
+    check('from', 'La fecha inicial "from" es obligatoria').not().isEmpty(),
+    check('to', 'La fecha final "to" es obligatoria').not().isEmpty(),
+    validarCampos
+], challengeGet );
 module.exports = router;
